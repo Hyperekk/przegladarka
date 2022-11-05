8277c694d45dce0602551f9de1ade3132311a43e
Hyperekk <barttolaj@gmail.com>
2022-11-05 12:59:27 +0100

    commit4 przycisk zmienia kolor

--- src/main/java/com/bartt/przegladarka/HelloController.java
+++ src/main/java/com/bartt/przegladarka/HelloController.java
@@ -6,6 +6,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.web.WebView;
 
+import java.util.stream.Stream;
+
 
 public class HelloController {
     public Button btn1;
@@ -23,20 +25,36 @@ public class HelloController {
     public void onClick1(ActionEvent actionEvent)
     {
         wb1.getEngine().load("https://www.onet.pl");
+
+        btn1.setDisable(true);
+
+        Stream.of(btn2, btn3, btn4).forEach(btn -> btn.setDisable(false));
     }
 
     public void onClick2(ActionEvent actionEvent)
     {
         wb1.getEngine().load("https://www.wp.pl");
+
+        btn2.setDisable(true);
+
+        Stream.of(btn1, btn3, btn4).forEach(btn -> btn.setDisable(false));
     }
 
     public void onClick3(ActionEvent actionEvent)
     {
         wb1.getEngine().load("https://www.interia.pl");
+
+        btn3.setDisable(true);
+
+        Stream.of(btn1, btn2, btn4).forEach(btn -> btn.setDisable(false));
     }
 
     public void onClick4(ActionEvent actionEvent)
     {
         wb1.getEngine().load("https://www.gazeta.pl/0,0.html");
+
+        btn4.setDisable(true);
+
+        Stream.of(btn1, btn3, btn2).forEach(btn -> btn.setDisable(false));
     }
 }
