7bb85ec27a4892bc21807f73e1cfe202c8da56fc
Hyperekk <barttolaj@gmail.com>
2022-11-11 22:00:39 +0100

    commit5 program 2.0 częsć mam dobrze ale jeszcze nie dziala bo mi brakuje czegos

--- src/main/java/com/bartt/przegladarka/HelloController.java
+++ src/main/java/com/bartt/przegladarka/HelloController.java
@@ -2,10 +2,16 @@ package com.bartt.przegladarka;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.web.WebView;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.stream.Stream;
 
 
@@ -16,15 +22,44 @@ public class HelloController {
     public Button btn4;
     public WebView wb1;
 
+    public String link;
+
+    public String nazwa;
+
     @FXML
-    public void initialize()
+    public void initialize() throws IOException
     {
+        Path path = Paths.get("C:/url.txt");
+        if (!Files.exists(path))
+            Files.createFile(path);
+
+        List<String> linie = Files.readAllLines(path);
+
+        for (String linia : linie)
+        {
+            int i = 0;
+
+            if(i>4)
+            {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("ERROR");
+                alert.setHeaderText("Przekroczyłes ilość lini url w tekscie");
+                alert.showAndWait();
+            }
+
+            String[] parts = linia.split("#");
+            Url url = new Url(parts[0], parts[1]);
+
+             link = (""+url.url);
+             nazwa = (""+url.strona);
+        }
+
         wb1.getEngine().load("https://google.com");
     }
 
     public void onClick1(ActionEvent actionEvent)
     {
-        wb1.getEngine().load("https://www.onet.pl");
+        wb1.getEngine().load(link);//https://www.onet.pl
 
         btn1.setDisable(true);
 
@@ -45,6 +80,7 @@ public class HelloController {
         wb1.getEngine().load("https://www.interia.pl");
 
         btn3.setDisable(true);
+
 
         Stream.of(btn1, btn2, btn4).forEach(btn -> btn.setDisable(false));
     }
---
+++ src/main/java/com/bartt/przegladarka/Url.java
@@ -0,0 +1,14 @@
+package com.bartt.przegladarka;
+
+public class Url
+{
+    public String strona;
+    public String url;
+
+    public Url(String strona, String url)
+    {
+        super();
+        this.strona = strona;
+        this.url = url;
+    }
+}
