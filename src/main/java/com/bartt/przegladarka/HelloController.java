7d3dcedb428651642fd1e705906e4a7f93255c6c
Hyperekk <barttolaj@gmail.com>
2022-11-12 13:25:42 +0100

    commit6 program 2.0 DZIAŁA

--- src/main/java/com/bartt/przegladarka/HelloController.java
+++ src/main/java/com/bartt/przegladarka/HelloController.java
@@ -22,23 +22,21 @@ public class HelloController {
     public Button btn4;
     public WebView wb1;
 
-    public String link;
+    public String[] link = new String[4];
 
-    public String nazwa;
+    public String[] nazwa = new String[4];
 
     @FXML
     public void initialize() throws IOException
     {
-        Path path = Paths.get("C:/url.txt");
+        Path path = Paths.get("C:/ZiomekczkiZiomy/url.txt");
         if (!Files.exists(path))
             Files.createFile(path);
 
         List<String> linie = Files.readAllLines(path);
-
+        int i = 0;
         for (String linia : linie)
         {
-            int i = 0;
-
             if(i>4)
             {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -50,8 +48,10 @@ public class HelloController {
             String[] parts = linia.split("#");
             Url url = new Url(parts[0], parts[1]);
 
-             link = (""+url.url);
-             nazwa = (""+url.strona);
+             link[i] = (""+url.url);
+             nazwa[i] = (""+url.strona);
+
+             i++;
         }
 
         wb1.getEngine().load("https://google.com");
@@ -59,27 +59,30 @@ public class HelloController {
 
     public void onClick1(ActionEvent actionEvent)
     {
-        wb1.getEngine().load(link);//https://www.onet.pl
+        wb1.getEngine().load(link[0]);//https://www.onet.pl
 
         btn1.setDisable(true);
+        btn1.setText(nazwa[0]);
 
         Stream.of(btn2, btn3, btn4).forEach(btn -> btn.setDisable(false));
     }
 
     public void onClick2(ActionEvent actionEvent)
     {
-        wb1.getEngine().load("https://www.wp.pl");
+        wb1.getEngine().load(link[1]);//"https://www.wp.pl"
 
         btn2.setDisable(true);
+        btn2.setText(nazwa[1]);
 
         Stream.of(btn1, btn3, btn4).forEach(btn -> btn.setDisable(false));
     }
 
     public void onClick3(ActionEvent actionEvent)
     {
-        wb1.getEngine().load("https://www.interia.pl");
+        wb1.getEngine().load(link[2]);//"https://www.interia.pl"
 
         btn3.setDisable(true);
+        btn3.setText(nazwa[2]);
 
 
         Stream.of(btn1, btn2, btn4).forEach(btn -> btn.setDisable(false));
@@ -87,9 +90,10 @@ public class HelloController {
 
     public void onClick4(ActionEvent actionEvent)
     {
-        wb1.getEngine().load("https://www.gazeta.pl/0,0.html");
+        wb1.getEngine().load(link[3]);//"https://www.gazeta.pl/0,0.html"
 
         btn4.setDisable(true);
+        btn4.setText(nazwa[3]);
 
         Stream.of(btn1, btn3, btn2).forEach(btn -> btn.setDisable(false));
     }
